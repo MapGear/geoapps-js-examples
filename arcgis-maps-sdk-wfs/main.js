429a36934eb3ddd5982401aff4979c2e3f1775f0
Wouter Goedhart <w.goedhart@mapgear.nl>
2023-06-13 09:07:55 +0200

    Add new wfs arcgis sample

--- arcgis-maps-sdk-wfs/main.js
+++ arcgis-maps-sdk-wfs/main.js
@@ -2,25 +2,29 @@ import WFSLayer from "@arcgis/core/layers/WFSLayer.js";
 import MapView from "@arcgis/core/views/MapView.js";
 import Map from "@arcgis/core/Map.js";
 
+// Deventer
+const location = [6.158202, 52.254178];
+
 // Set up map and view
 const map = new Map({
     basemap: "gray-vector"
 });
 
+// Create mapview
 const view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [6.158202, 52.254178], // Deventer
+    center: location,
     zoom: 12,
     popup: {
-        defaultPopupTemplateEnabled: true // popup will be enabled on the wfslayer
+        defaultPopupTemplateEnabled: true
     }
 });
 
-// Create and add a WFSLayer to the map
+// Construct a WFSLayer instance and add it to the map
 const layer = new WFSLayer({
-    url: "https://demo.geoapps.nl/services/wfs/?token=4b4154dc0b7f4c029a2b4d07264f43f9&", // When used with a token, please change the url to: https://demo.geoapps.nl/services/wfs/?token=<token>&" and replace <token> with your token
-    name: "gebouwen", // Put here the name of your layer that is assigned in the Layer Service in GeoApps
+    url: "https://demo.geoapps.nl/services/wfs", // When used with a token, please change the url to: "https://demo.geoapps.nl/services/wfs/?token=<token>&" (without quotes) and replace <token> with your token
+    name: "panden", // Put here the name of your layer that is assigned in the Layer Service in GeoApps
     copyright: "GeoApps Demo Service"
 });
 
